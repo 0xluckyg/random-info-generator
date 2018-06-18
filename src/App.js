d92791e06fa7ce7b692119aeb745029cb7c66cf2
Scott Cho <scottsgcho@gmail.com>
2018-06-18 23:13:24 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      name: ''
+    }
+
+    this.createInfo = this.createInfo.bind(this)
+  }
+
+
+	createInfo() {
+		let url = 'https://randomuser.me/api/?nat=us'
+    axios.get(url)
+      .then(res => {
+        let info = res.data.results[0]        
+        this.setState({
+          name: info.name.first + " " + info.name.last
+        })
+      })
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<button style={divStyle} onClick={this.createInfo}>Create info</button>
+        <p>{this.state.name}</p>
+			</div>
+		);
+	}  
+}
+
+const divStyle = {
+  color: 'blue',
+  width: '200px',
+  height: '50px'
+};
+
+export default App;
